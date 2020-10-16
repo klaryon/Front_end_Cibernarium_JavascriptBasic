79191c4bd5158fd40f995c65ac4632015d3d1d3a
klaryon <klary.ardon@gmail.com>
2020-10-16 11:04:35 +0200

    update files

--- 02. Repeated_Letters/phase 1/js/scripts.js
+++
@@ -1,7 +0,0 @@
-function repeat() { 
-    let nameLetter = ['c', 'l', 'a', 'r', 'i', 's', 's', 'a'];
-
-        for (let i = 0; i < nameLetter.length; i++) {
-            document.getElementById("demo").innerHTML += nameLetter[i];
-        }
-    }
