44088141abf9ad04efbcb0201b31e1eeb7f8ad66
klaryon <klary.ardon@gmail.com>
2020-11-09 14:44:02 +0100

    subscribe form validation added

--- 03. Form Validation/js/validate.js
+++ 03. Form Validation/js/validate.js
@@ -1,7 +1,8 @@
 //these variables will be used in the event listener to apply or remove input's invalid color
-const form = document.getElementById('myFormId');
-const form2 = document.getElementById('myFormId2');
-const form3 = document.getElementById('myFormId3');
+const form = document.getElementById('myFormId'); /* id <form> for ---> function searchValidate() */
+const form2 = document.getElementById('myFormId2'); /* for ---> function signinValidate() */
+const form3 = document.getElementById('myFormId3'); /* for ---> function registerValidate()  */
+const form4 = document.getElementById('myFormId4'); /* for ---> function subscribeValidate()  */
 
 //search function
 function searchValidate() {
@@ -10,23 +11,23 @@ function searchValidate() {
   form.classList.remove('is-invalid'); //removes indicated classes. Removing a non-existing class doesn't cause error.
   form_event(form); //.addEventListener function
   
-  // let inputSearch = document.getElementById('inputSearch'); 
-  let inputSearch = document.forms["myForm"]["inputSearch"];
+  // let inputSearch = document.getElementById('inputSearch');  /* other way of getting value */
+  let inputSearch = document.forms["myForm"]["inputSearch"]; /* [name <form>] [id <element>] */
 
   if(inputSearch.value == "") {
 		inputSearch.classList.add("is-invalid"); //adds atributes to existing class. It is only for lecture. 
 		document.getElementById("errorSearch").textContent = "Required field"; //.textContent returns text of the specified node
 		acumErrores ++; //adds accumulated errors
-	}else if(inputSearch.value.length < 3) { 
+	}else if(inputSearch.value.length < 3) { /* .value obtains data entered by user */
     inputSearch.classList.add("is-invalid");
 		document.getElementById("errorSearch").textContent = "Enter at least 3 characters"; 
 		acumErrores ++;
   }
 
   if (acumErrores > 0){
-    return false;
+    return false; /* do not accepts "submit" and do not sends information to the server */
     }else{
-    return true;
+    return true; /* accepts "submit" and sends information to the server*/
   }
 }
 
@@ -89,11 +90,11 @@ function registerValidate() {
   let inputEmail2 = document.forms["myForm3"]["inputEmail2"];
   let inputPassword2 = document.forms["myForm3"]["inputPassword2"];
   let inputRepeatPassword = document.forms["myForm3"]["inputRepeatPassword"];
-	let inputAddress = document.forms["myForm3"]["inputAddress"];
-	let inputProvince = document.forms["myForm3"]["inputProvince"];
-	let inputCity = document.forms["myForm3"]["inputCity"];
-	let inputZip = document.forms["myForm3"]["inputZip"];
-	let gridCheck = document.forms["myForm3"]["gridCheck"];
+  let inputAddress = document.forms["myForm3"]["inputAddress"];
+  let inputProvince = document.forms["myForm3"]["inputProvince"];
+  let inputCity = document.forms["myForm3"]["inputCity"];
+  let inputZip = document.forms["myForm3"]["inputZip"];
+  let gridCheck = document.forms["myForm3"]["gridCheck"];
 
   if(inputEmail2.value == "") {
 		inputEmail2.classList.add("is-invalid");
@@ -166,11 +167,37 @@ function registerValidate() {
 	}
 }
 
+function subscribeValidate() {
+	let acumErrores = 0;
+	  
+	form4.classList.remove('is-invalid'); //removes indicated classes. Removing a non-existing class doesn't cause error.
+	form_event(form4); //.addEventListener function
+	
+	// let inputSearch = document.getElementById('inputSearch'); 
+	let inputEmail3 = document.forms["myForm4"]["inputEmail3"];
+  
+	if(inputEmail3.value == "") {
+		inputEmail3.classList.add("is-invalid");
+		document.getElementById("errorSubscribe").textContent = "Required field";
+        acumErrores ++;
+    }else if(!validar_email(inputEmail3.value)){
+		inputEmail3.classList.add("is-invalid");
+		document.getElementById("errorSubscribe").textContent = "Please enter a valid email address";
+		acumErrores ++;
+	}
+  
+	if (acumErrores > 0){
+	  return false;
+	  }else{
+	  return true;
+	}
+  }
+
 //Removes invalid color from input when valid
-function form_event(formEvent) {
-  formEvent.addEventListener('blur', (event) => {
-    console.log(event);
-    if(event.target.value!='') event.target.classList.remove('is-invalid');
+function form_event(formEvent) { /* refactor --> DRY principle */
+  formEvent.addEventListener('blur', (event) => { /* blur is triggered when the element has lost it's focus by cliking outside of the <input> box. */
+    console.log(event); /* prints event to console */
+    if(event.target.value!='') event.target.classList.remove('is-invalid');  /* if (<element> not empty) then remove 'is-invalid' from class. Event.target searchs for the element where the action is triggered from */ 
   }, true);
 }
 
@@ -179,3 +206,6 @@ function validar_email(email) {
 	var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 	return regex.test(email) ? true : false;
 }
+
+/* Blur event: https://developer.mozilla.org/es/docs/Web/Events/blur */
+/* event.target: https://www.w3schools.com/jsref/event_target.asp */
